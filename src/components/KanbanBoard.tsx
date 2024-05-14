a81fe2214030bdd4e169f59616556eb00ba73f77
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-05-14 14:04:18 -0400

    First Commit

---
+++ src/components/KanbanBoard.tsx
@@ -0,0 +1,57 @@
+import PlusIcon from '../icons/PlusIcon';
+import {useState} from "react";
+
+function KanbanBoard() {
+  const [columns, setColumns] = useState([]);
+  return ( 
+  <div 
+  className="
+    m-auto
+    flex
+    min-h-screen
+    w-full
+    items-center
+    overflow-x-auto
+    overflow-y-hidden
+    px-[40px]
+  "
+  >
+    <div className="m-auto">
+     <button 
+     onClick={() => {
+      createNewColumn();
+     }}
+      className="
+      h-[60px]
+      w-[350px]
+      min-w-[350px]
+      cursor-pointer
+      rounded-lg
+      bg-mainBackgroundColor
+      border-2
+      border-columnBackgroundColor
+      p-4
+      ring-rose-500
+      hover:ring-2
+      flex
+      gap-2
+    "
+    >
+      <PlusIcon/> 
+      Add Column
+      </button>
+    </div>
+  </div>
+ ); 
+
+
+
+
+    function createNewColumn() {}
+}
+
+
+
+
+
+export default KanbanBoard
